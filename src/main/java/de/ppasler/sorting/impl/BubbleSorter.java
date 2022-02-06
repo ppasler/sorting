64dbf010b7c9c378318da4432dffa5d5411849d3
ppasler <ppasler@seibert-media.net>
2022-02-06 17:03:15 +0100

    added bubble sorter

---
+++ src/main/java/de/ppasler/sorting/impl/BubbleSorter.java
@@ -0,0 +1,23 @@
+package de.ppasler.sorting.impl;
+
+import static de.ppasler.sorting.util.GenericsComparer.gt;
+import static java.util.Collections.swap;
+
+import java.util.List;
+
+import de.ppasler.sorting.Sorter;
+
+public class BubbleSorter<T extends Comparable<T>> implements Sorter<T> {
+
+	@Override
+	public List<T> sort(final List<T> list) {
+		for (int i = list.size(); i > 1; i--){
+			for (int j = 0; j < i-1; j++){
+				if (gt(list.get(j), list.get(j + 1))){
+					swap(list, j, j + 1);
+				}
+			}
+		}
+		return list;
+	}
+}
---
+++ src/test/java/de/ppasler/sorting/impl/BubbleSorterTest.java
@@ -0,0 +1,10 @@
+package de.ppasler.sorting.impl;
+
+import de.ppasler.sorting.SorterTest;
+
+class BubbleSorterTest extends SorterTest {
+
+	BubbleSorterTest() throws Exception {
+		super(BubbleSorter.class);
+	}
+}
